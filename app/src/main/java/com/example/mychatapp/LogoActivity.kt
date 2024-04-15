a51c18833564a94f36d8409e9d69e4a3f53bf2ec
wang <guanmin2100@gmail.com>
2024-04-16 00:01:40 +0800

    1. Add colors in colors.xml and themes.xml. 2. Set LogoActivity's UI and add functionality to the progressbar.

--- app/src/main/java/com/example/mychatapp/LogoActivity.kt
+++ app/src/main/java/com/example/mychatapp/LogoActivity.kt
@@ -1,11 +1,22 @@
 package com.example.mychatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.mychatapp.databinding.ActivityLogoBinding
 
 class LogoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_logo)
+        binding = ActivityLogoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        },2000)
+
     }
 }
