cc44e39811ae1c54cf8d9d47675505712ec25990
wang <guanmin2100@gmail.com>
2024-04-16 00:50:29 +0800

    SignInActivity UI OK.

--- app/src/main/java/com/example/mychatapp/SignInActivity.kt
+++ app/src/main/java/com/example/mychatapp/SignInActivity.kt
@@ -2,10 +2,13 @@ package com.example.mychatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mychatapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/mychatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/mychatapp/SignUpActivity.kt
@@ -2,10 +2,13 @@ package com.example.mychatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mychatapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
