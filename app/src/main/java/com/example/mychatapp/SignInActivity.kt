be728d1151aebdf5c658136f057dc3c9fe36c201
wang <guanmin2100@gmail.com>
2024-04-16 02:08:49 +0800

    Sign in and Sign up are finished.

--- app/src/main/java/com/example/mychatapp/SignInActivity.kt
+++ app/src/main/java/com/example/mychatapp/SignInActivity.kt
@@ -3,19 +3,43 @@ package com.example.mychatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mychatapp.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
 
         binding.btnSignUp.setOnClickListener {
             Intent(this, SignUpActivity::class.java).apply {
                 startActivity(this)
             }
         }
+        binding.btnSignIn.setOnClickListener {
+            val gmail = binding.gmail.text.toString()
+            val password = binding.password.text.toString()
+
+            signIn(gmail, password)
+        }
+    }
+    private fun signIn(gmail:String, password:String){
+        auth.signInWithEmailAndPassword(gmail, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/mychatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/mychatapp/SignUpActivity.kt
@@ -1,14 +1,42 @@
 package com.example.mychatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.mychatapp.databinding.ActivitySignInBinding
 import com.example.mychatapp.databinding.ActivitySignUpBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnSignUp.setOnClickListener {
+            val gmail = binding.gmail.text.toString()
+            val password = binding.password.text.toString()
+            signUp(gmail, password)
+        }
+    }
+    private fun signUp(gmail:String, password:String){
+        auth.createUserWithEmailAndPassword(gmail, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                } else {
+                    Toast.makeText(this, "account create failed", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
